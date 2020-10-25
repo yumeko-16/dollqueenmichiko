f4a978e2ade8f8a57c83c013a606b54f30155573
Sihoo-milk <>
2020-10-25 18:28:01 +0900

    グローバルナビのレスポンシブ設定

--- header.php
+++ header.php
@@ -29,7 +29,7 @@
         <div class="header__logo">
           <h1><a href="<?php echo home_url() ?>" class="header__img"><img src="<?php echo get_template_directory_uri(); ?>/images/common/logo.png" alt="人形作家 みちこ"></a></h1>
         </div>
-        <nav>
+        <nav class="header__nav">
           <?php
           $args = array(
             'menu' => 'global-navigation',
@@ -38,34 +38,6 @@
           );
           wp_nav_menu($args);
           ?>
-        </nav>
-        <nav class="header__sp-nav">
-          <ul>
-            <li class="header__sp-item">
-              <a href="/">
-                <span class="header__nav-txt header__nav-txt--en">HOME</span>
-                <span class="header__nav-txt header__nav-txt--jp">ホーム</span>
-              </a>
-            </li>
-            <li class="header__sp-item">
-              <a href="">
-                <span class="header__nav-txt header__nav-txt--en">NEWS</span>
-                <span class="header__nav-txt header__nav-txt--jp">ニュース</span>
-              </a>
-            </li>
-            <li class="header__sp-item">
-              <a href="">
-                <span class="header__nav-txt header__nav-txt--en">DOLL</span>
-                <span class="header__nav-txt header__nav-txt--jp">作品</span>
-              </a>
-            </li>
-            <li class="header__sp-item">
-              <a href="">
-                <span class="header__nav-txt header__nav-txt--en">CONTACT</span>
-                <span class="header__nav-txt header__nav-txt--jp">お問い合わせ</span>
-              </a>
-            </li>
-          </ul>
         </nav>
         <div id="js-burger" class="header__burger">
           <div class="header__burger-inner">
