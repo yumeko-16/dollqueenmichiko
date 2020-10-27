679bb3adf227794fa6060c551dc9e83ad02cb457
Sihoo-milk <>
2020-10-27 21:35:38 +0900

    トップページのカテゴリー別記事表示の設定を修正。カテゴリー一覧ページへのリンクボタンをカテゴリー名から指定テキストに変更。

--- front-page.php
+++ front-page.php
@@ -1,20 +1,94 @@
 <?php get_header(); ?>
 
 <main class="main">
-  <?php if (is_home()) : ?>
-    <section class="section mv">
-      <div class="section__inner">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/home/main.png" alt="メインビジュアル" class="mv__img">
+  <section class="section">
+    <div class="section__inner">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/home/main.png" alt="メインビジュアル" class="mv__img">
+    </div>
+  </section>
+
+  <?php
+  $categorys = array(1);
+  for ($i = 0; $i < count($categorys); $i++) :
+  ?>
+  <section class="section">
+    <div class="section__inner">
+      <h2 class="title"><?php echo esc_html(get_catname($categorys[$i])); ?></h2>
+      <div class="group">
+      <?php
+      query_posts('showposts=4&cat='.$categorys[$i]);
+      if (have_posts()) :
+        while (have_posts()) : the_post();
+      ?>
+        <article class="article">
+          <a href="<?php the_permalink() ?>">
+            <time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('Y.m.d(D)'); ?></time>
+            <h3><?php the_title(); ?></h3>
+          </a>
+        </article>
+        <?php endwhile; ?>
       </div>
-    </section>
-    <section class="section news">
-      <div class="section__inner">
-        <h2 class="title">NEWS</h2>
-        <?php get_template_part('loop', 'news'); ?>
-        <?php echo the_category(); ?>
+      <?php else: ?>
+        <li><?php echo esc_html(get_catname($categorys[$i]))."はまだありません。"; ?></li>
+      </ul>
+      <?php endif; ?>
+
+      <?php
+      // 指定したカテゴリーの ID を取得
+      $category_id = get_cat_ID( 'NEWS' );
+      // このカテゴリーの URL を取得
+      $category_link = get_category_link( $category_id );
+      ?>
+      <!-- このカテゴリーへのリンクを出力 -->
+      <a href="<?php echo esc_url( $category_link ); ?>" title="NEWS">MORE</a>
+    </div>
+  </section>
+  <?php endfor; ?>
+
+  <?php
+  $categorys = array(4);
+  for ($i = 0; $i < count($categorys); $i++) :
+  ?>
+  <section class="section">
+    <div class="section__inner">
+      <h2 class="title"><?php echo esc_html(get_catname($categorys[$i])); ?></h2>
+      <div class="group">
+      <?php
+      query_posts('showposts=4&cat='.$categorys[$i]);
+      if (have_posts()) :
+        while (have_posts()) : the_post();
+      ?>
+        <article class="article">
+          <a href="<?php the_permalink() ?>">
+            <figure>
+            <?php if ( has_post_thumbnail() ): ?>
+              <?php the_post_thumbnail('thumbnail', array('class' => 'article__thumb')); ?>
+            <?php else: ?>
+              <img src="<?php echo get_template_directory_uri(); ?>/images/common/noimage_180x180.png" alt="No Image" class="article__thumb">
+            <?php endif; ?>
+            </figure>
+            <time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('Y.m.d(D)'); ?></time>
+            <h3><?php the_title(); ?></h3>
+          </a>
+        </article>
+        <?php endwhile; ?>
       </div>
-    </section>
-  <?php endif; ?>
+      <?php else: ?>
+        <li><?php echo esc_html(get_catname($categorys[$i]))."はまだありません。"; ?></li>
+      </ul>
+      <?php endif; ?>
+
+      <?php
+      // 指定したカテゴリーの ID を取得
+      $category_id = get_cat_ID( 'DOLL' );
+      // このカテゴリーの URL を取得
+      $category_link = get_category_link( $category_id );
+      ?>
+      <!-- このカテゴリーへのリンクを出力 -->
+      <a href="<?php echo esc_url( $category_link ); ?>" title="DOLL">MORE</a>
+    </div>
+  </section>
+  <?php endfor; ?>
 </main>
 
 <?php get_footer(); ?>
--- index.php
+++ index.php
@@ -4,7 +4,11 @@
   <section class="section">
     <div class="section__inner">
       <h2 class="title"><?php wp_title(''); ?></h2>
-      <?php get_template_part('loop', 'news'); ?>
+      <?php if ( is_category('news') ): ?>
+        <?php get_template_part('loop', 'news'); ?>
+      <?php elseif( is_category('doll') ): ?>
+        <?php get_template_part('loop', 'doll'); ?>
+      <?php endif; ?>
       <div id="post-<?php the_ID(); ?>"></div>
     </div>
   </section>
