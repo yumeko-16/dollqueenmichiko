1c3f82c0dc14956f121ca138c75155d646529539
Sihoo-milk <>
2020-10-29 01:16:17 +0900

    記事詳細ページのコメント一覧にページングを設定

--- functions.php
+++ functions.php
@@ -13,3 +13,21 @@ add_theme_support( 'menus' );
  * 画像挿入時にwidthとheightを削除する
  */
 add_filter('wp_img_tag_add_width_and_height_attr', '__return_false');
+
+/**
+ * コメントフォームの「名前」「メールアドレス」「ウェブサイト」を削除
+ */
+add_filter('comment_form_default_fields', 'my_comment_form_default_fields');
+function my_comment_form_default_fields( $args ) {
+  $args['author'] = ''; //「名前」を削除
+  $args['email'] = ''; //「メールアドレス」を削除
+  $args['url'] = ''; //「ウェブサイト」を削除
+  return $args;
+}
+// 「メールアドレスが公開されることはありません。 * が付いている欄は必須項目です」の文言を削除
+add_filter("comment_form_defaults", "my_comment_notes_before");
+function my_comment_notes_before($defaults)
+{
+  $defaults['comment_notes_before'] = '';
+  return $defaults;
+}
--- single.php
+++ single.php
@@ -4,26 +4,26 @@
   <section class="section">
     <div class="section__inner">
       <div class="content">
-        <?php
-        if (have_posts()) :
-          while (have_posts()) : the_post();
-        ?>
-            <article <?php post_class('entry'); ?>>
-              <h3><span><?php the_title(); ?></span></h3>
-              <time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('Y.m.d(D)'); ?></a></time>
-              <?php the_content(); ?>
+      <?php
+      if (have_posts()) :
+        while (have_posts()) : the_post();
+      ?>
+        <article <?php post_class('entry'); ?>>
+          <h3><span><?php the_title(); ?></span></h3>
+          <time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('Y.m.d(D)'); ?></a></time>
+          <?php the_content(); ?>
 
-              <?php comments_template(); ?>
+          <?php comments_template(); ?>
 
-              <nav>
-                <span><?php previous_post_link('%link'); ?></span>
-                <span><?php next_post_link('%link'); ?></span>
-              </nav>
-            </article>
-        <?php
-          endwhile;
-        endif;
-        ?>
+          <nav>
+            <span><?php previous_post_link('%link'); ?></span>
+            <span><?php next_post_link('%link'); ?></span>
+          </nav>
+        </article>
+      <?php
+        endwhile;
+      endif;
+      ?>
       </div>
     </div>
   </section>
