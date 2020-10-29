9378c0b822a54ae483427e7ef90f6bcfb622f453
Sihoo-milk <>
2020-10-30 00:24:33 +0900

    footerのテキスト・スタイルを修正

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
     <footer class="footer">
       <div class="footer__inner">
-        <small>&copy; 2020 Michiko.Maeno</small>
+        <small>&copy; 2020 Tomohiro.Maeno</small>
       </div>
     </footer>
   </div>
