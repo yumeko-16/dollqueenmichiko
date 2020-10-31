648b47c3fd68b165e302afbf823ba109ca8cbd5a
Sihoo-milk <>
2020-10-31 20:34:18 +0900

    コメントフォームのHTMLをお問い合わせフォームと同じになるように修正

--- functions.php
+++ functions.php
@@ -19,6 +19,7 @@ add_filter('wp_img_tag_add_width_and_height_attr', '__return_false');
  */
 add_filter('comment_form_default_fields', 'my_comment_form_default_fields');
 function my_comment_form_default_fields( $args ) {
+  $args['email'] = '';
   $args['url'] = ''; //「ウェブサイト」を削除
   return $args;
 }
