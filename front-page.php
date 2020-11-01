10432a5567557f08423f722dd9444cb4fbd405c2
Sihoo-milk <>
2020-11-01 19:15:20 +0900

    トップページに表示される記事の指定をカテゴリーIDからカテゴリースラッグ名に変更

--- front-page.php
+++ front-page.php
@@ -7,30 +7,30 @@
     </div>
   </section>
 
-  <?php
-  $categorys = array(1);
-  for ($i = 0; $i < count($categorys); $i++) :
-  ?>
-    <section class="section">
-      <div class="section__inner">
-        <h2 class="title news__title"><?php echo esc_html(get_catname($categorys[$i])); ?></h2>
-        <div class="content">
-          <ul class="group group--news">
-            <?php
-            query_posts('showposts=4&cat=' . $categorys[$i]);
-            if (have_posts()) :
-              while (have_posts()) : the_post();
-            ?>
-                <li class="article article--news">
-                  <a href="<?php the_permalink() ?>">
-                    <time datetime="<?php the_time('Y-m-d'); ?>" class="news__time"><?php the_time('Y.m.d(D)'); ?></time>
-                    <h3 class="article__heading"><?php the_title(); ?></h3>
-                  </a>
-                </li>
-              <?php endwhile; ?>
-          </ul>
+  <section class="section">
+    <div class="section__inner">
+      <h2 class="title news__title">NEWS</h2>
+      <div class="content">
+        <ul class="group group--news">
+        <?php
+        $args = array(
+          'category_name' => 'news',
+          'posts_per_page' => 4
+        );
+        $the_query = new WP_Query($args);
+        if ($the_query -> have_posts()) :
+          while ($the_query -> have_posts()) : $the_query -> the_post();
+        ?>
+          <li class="article article--news">
+            <a href="<?php the_permalink() ?>">
+              <time datetime="<?php the_time('Y-m-d'); ?>" class="news__time"><?php the_time('Y.m.d(D)'); ?></time>
+              <h3 class="article__heading"><?php the_title(); ?></h3>
+            </a>
+          </li>
+          <?php endwhile; ?>
+        </ul>
         <?php else : ?>
-          <li><?php echo "記事はまだありません。"; ?></li>
+            <li><?php echo "記事はまだありません。"; ?></li>
           </ul>
         <?php endif; ?>
 
@@ -44,42 +44,41 @@
         <div class="content__center">
           <a href="<?php echo esc_url($category_link); ?>" title="NEWS" class="content__btn">VIEW MORE</a>
         </div>
-        </div>
       </div>
-    </section>
-  <?php endfor; ?>
+    </div>
+  </section>
 
-  <?php
-  $categorys = array(4);
-  for ($i = 0; $i < count($categorys); $i++) :
-  ?>
-    <section class="section">
-      <div class="section__inner">
-        <h2 class="title doll__title"><?php echo esc_html(get_catname($categorys[$i])); ?></h2>
-        <ul class="group">
-          <?php
-          query_posts('showposts=4&cat=' . $categorys[$i]);
-          if (have_posts()) :
-            while (have_posts()) : the_post();
-          ?>
-              <li class="article">
-                <a href="<?php the_permalink() ?>">
-                  <figure>
-                    <?php if (has_post_thumbnail()) : ?>
-                      <?php the_post_thumbnail('thumbnail', array('class' => 'article__thumb')); ?>
-                    <?php else : ?>
-                      <img src="<?php echo get_template_directory_uri(); ?>/images/common/noimage_180x180.png" alt="No Image" class="article__thumb">
-                    <?php endif; ?>
-                  </figure>
-                  <time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('Y.m.d(D)'); ?></time>
-                  <h3><?php the_title(); ?></h3>
-                </a>
-              </li>
-            <?php endwhile; ?>
-        </ul>
+  <section class="section">
+    <div class="section__inner">
+      <h2 class="title doll__title">DOLL</h2>
+      <ul class="group">
+      <?php
+      $args = array(
+        'category_name' => 'doll',
+        'posts_per_page' => 4
+      );
+      $the_query = new WP_Query($args);
+      if ($the_query -> have_posts()) :
+        while ($the_query -> have_posts()) : $the_query -> the_post();
+      ?>
+        <li class="article">
+          <a href="<?php the_permalink() ?>">
+            <figure>
+              <?php if (has_post_thumbnail()) : ?>
+                <?php the_post_thumbnail('thumbnail', array('class' => 'article__thumb')); ?>
+              <?php else : ?>
+                <img src="<?php echo get_template_directory_uri(); ?>/images/common/noimage_180x180.png" alt="No Image" class="article__thumb">
+              <?php endif; ?>
+            </figure>
+            <time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('Y.m.d(D)'); ?></time>
+            <h3><?php the_title(); ?></h3>
+          </a>
+        </li>
+        <?php endwhile; ?>
+      </ul>
       <?php else : ?>
         <li><?php echo "記事はまだありません。"; ?></li>
-        </ul>
+      </ul>
       <?php endif; ?>
 
       <?php
@@ -88,12 +87,12 @@
       // このカテゴリーの URL を取得
       $category_link = get_category_link($category_id);
       ?>
+      
       <div class="content__center">
         <a href="<?php echo esc_url($category_link); ?>" title="DOLL" class="content__btn">VIEW MORE</a>
       </div>
-      </div>
-    </section>
-  <?php endfor; ?>
+    </div>
+  </section>
 
   <section class="section">
     <div class="section__inner clearfix">
