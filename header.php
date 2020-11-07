05f72634471ab46825845758ae5cc74a23ec1f6a
Sihoo-milk <>
2020-11-07 23:55:27 +0900

    不要なスタイルの指定を削除。検索用metaタグの追加。ロゴimgのaltテキストを修正。

--- header.php
+++ header.php
@@ -5,6 +5,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="keywords" content="Doll, Writter, MICHIKO, 人形, 作家, 前野美智子">
+  <meta name="description" content="人形作家前野美智子のWebサイト。創作人形で表現する独自の世界観を覗いてください。">
   <title><?php if ( !is_home() ) { wp_title(' | ', true, 'right'); } ?><?php bloginfo('name'); ?></title>
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css" type="text/css" />
   <link href="https://fonts.googleapis.com/css2?family=Noto+Sans&family=Noto+Sans+JP:wght@700;900&display=swap" rel="stylesheet">
@@ -19,6 +21,7 @@
   wp_enqueue_script('doll-common', get_template_directory() . '/js/common.js');
   wp_head();
   ?>
+  <meta name="google-site-verification" content="h39whFYYbbl1RpERy9_nKsd1w0E66pf213tGprpb1M4" />
 </head>
 
 <body>
@@ -26,7 +29,7 @@
     <header class="header">
       <div class="header__bar"></div>
       <div class="header__inner">
-        <h1><a href="<?php echo home_url() ?>" class="header__logo"><img class="header__img" src="<?php echo get_template_directory_uri(); ?>/images/common/logo.png" alt="人形作家 みちこ"></a></h1>
+        <h1><a href="<?php echo home_url() ?>" class="header__logo"><img class="header__img" src="<?php echo get_template_directory_uri(); ?>/images/common/logo.png" alt="Doll Writter MICHIKO"></a></h1>
         <nav class="header__nav">
           <?php
           $args = array(
