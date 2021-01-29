487a492861f39646fd652299b3ef7660fc16eb64
Sihoo-milk <>
2021-01-29 14:38:30 +0900

    DOLLセクションとCOLUMNセクションのレスポンシブ構成を変更

--- front-page.php
+++ front-page.php
@@ -80,16 +80,18 @@
         while ($the_query -> have_posts()) : $the_query -> the_post();
       ?>
         <li class="article">
-          <a href="<?php the_permalink(); ?>">
-            <figure>
+          <a href="<?php the_permalink(); ?>" class="article__link">
+            <figure class="article__figure">
               <?php if (has_post_thumbnail()) : ?>
                 <?php the_post_thumbnail('thumbnail', array('class' => 'article__thumb')); ?>
               <?php else : ?>
                 <img src="<?php echo get_template_directory_uri(); ?>/images/common/noimage_180x180.png" alt="No Image" class="article__thumb">
               <?php endif; ?>
             </figure>
-            <time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('Y.m.d(D)'); ?></time>
-            <h3><?php the_title(); ?></h3>
+            <div class="article__caption">
+              <time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('Y.m.d(D)'); ?></time>
+              <h3><?php the_title(); ?></h3>
+            </div>
           </a>
         </li>
         <?php endwhile; ?>
@@ -126,16 +128,18 @@
         while ($the_query -> have_posts()) : $the_query -> the_post();
       ?>
         <li class="article">
-          <a href="<?php the_permalink(); ?>">
-            <figure>
+          <a href="<?php the_permalink(); ?>" class="article__link">
+            <figure class="article__figure">
               <?php if (has_post_thumbnail()) : ?>
                 <?php the_post_thumbnail('thumbnail', array('class' => 'article__thumb')); ?>
               <?php else : ?>
                 <img src="<?php echo get_template_directory_uri(); ?>/images/common/noimage_180x180.png" alt="No Image" class="article__thumb">
               <?php endif; ?>
             </figure>
-            <time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('Y.m.d(D)'); ?></time>
-            <h3><?php the_title(); ?></h3>
+            <div class="article__caption">
+              <time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('Y.m.d(D)'); ?></time>
+              <h3><?php the_title(); ?></h3>
+            </div>
           </a>
         </li>
         <?php endwhile; ?>
